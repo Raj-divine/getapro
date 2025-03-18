63df387c88cb7b7db2da14881a8fe4dffc84acda
Raj-divine <tarunkush0510@gmail.com>
2025-03-18 16:40:37 +0530

    removed the category fields from professional signup modal and added password field

---
+++ components/OnBoardingButtons.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+
+export default function OnBoardingButtons() {
+    return <div className='flex mt-8'>
+        <Button variant='primary' className='max-sm:mr-3 max-sm:h-9 sm:mr-2'>
+            Find a Professional <ArrowRightIcon className='ml-1 ' />
+        </Button>
+
+        <Button variant='secondary' className='max-sm:h-9' asChild>
+            <Link href="/professional-signup">
+                Join as Professional
+            </Link>
+        </Button>
+    </div>;
+}
