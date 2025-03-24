a5b68d152c47be2b9051fd8db8fffe6e5bb4020f
Navneet-divine <navneetkush0102@gmail.com>
2025-03-24 12:38:08 +0530

    moved files into page component folder

--- app/professional-signup/components/SignUpForm.tsx
+++ app/professional-signup/components/SignUpForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
-import { Button } from '../ui/button';
+import { Button } from '../../../components/ui/button';
 import {
   Form,
   FormControl,
@@ -11,15 +11,15 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '../ui/form';
-import { Input } from '../ui/input';
+} from '../../../components/ui/form';
+import { Input } from '../../../components/ui/input';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '../ui/card';
+} from '../../../components/ui/card';
 
 /*
 const professionalCategories = [
--- app/professional-signup/page.tsx
+++ app/professional-signup/page.tsx
@@ -1,19 +1,19 @@
-import Footer from "@/components/layout/Footer";
-import Navbar from "@/components/layout/Navbar";
-import BenefitSection from "@/components/ProfessionalSignupPage/BenefitSection";
-import FAQCardSection from "@/components/ProfessionalSignupPage/FAQCardSection";
-import FormSection from "@/components/ProfessionalSignupPage/FormSection";
+import Footer from '@/components/layout/Footer';
+import Navbar from '@/components/layout/Navbar';
+import BenefitSection from '@/app/professional-signup/components/BenefitSection';
+import FAQCardSection from '@/app/professional-signup/components/FAQCardSection';
+import FormSection from '@/app/professional-signup/components/FormSection';
 
 export default function ProfessionalSignupPage() {
-    return (
-        <>
-            <Navbar />
-            <main>
-                <FormSection />
-                <FAQCardSection />
-                <BenefitSection />
-            </main>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <main>
+        <FormSection />
+        <FAQCardSection />
+        <BenefitSection />
+      </main>
+      <Footer />
+    </>
+  );
 }
