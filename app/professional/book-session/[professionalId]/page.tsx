100b94ac7e08019dc2239e18741a02a4f8a78b5d
Navneet-divine <navneetkush0102@gmail.com>
2025-03-27 14:04:03 +0530

    make booking-page responsive for mobile screens

--- app/professional/book-session/[professionalId]/page.tsx
+++ app/professional/book-session/[professionalId]/page.tsx
@@ -1,30 +1,52 @@
-import Footer from "@/components/layout/Footer";
-import Navbar from "@/components/layout/Navbar";
-import { createClient } from "@/utils/supabase/server";
-import DetailsSection from "./components/DetailsSection";
-import ReviewSection from "./components/ReviewSection";
-import BookingSection from "./components/BookingSection";
+import Footer from '@/components/layout/Footer';
+import Navbar from '@/components/layout/Navbar';
+import { createClient } from '@/utils/supabase/server';
+import DetailsSection from './components/DetailsSection';
+import ReviewSection from './components/ReviewSection';
+import BookingSection from './components/BookingSection';
 
-export default async function BookSessionPage({ params }: { params: Promise<{ professionalId: string }> }) {
-    const { professionalId } = await params;
-    const supabase = await createClient();
-    const { data, error } = await supabase.from('professionals').select('*, public_user_names(first_name, last_name)').eq('professional_id', professionalId).single();
-    // TODO: Add error handling
-    if (!data || error) {
-        throw new Error('Failed to fetch professional');
-    }
+export default async function BookSessionPage({
+  params,
+}: {
+  params: Promise<{ professionalId: string }>;
+}) {
+  const { professionalId } = await params;
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from('professionals')
+    .select('*, public_user_names(first_name, last_name)')
+    .eq('professional_id', professionalId)
+    .single();
+  // TODO: Add error handling
+  if (!data || error) {
+    throw new Error('Failed to fetch professional');
+  }
 
-    return <>
-        <Navbar />
-        <main>
-            <div className="flex gap-5 px-16 py-5 h-[105vh]">
-                <div className="w-[70%] flex flex-col gap-5">
-                    <DetailsSection data={data} />
-                    <ReviewSection />
-                </div>
-                <BookingSection />
-            </div>
-        </main>
-        <Footer />
-    </>;
+  return (
+    <>
+      <Navbar />
+      <main>
+        <div className='flex flex-col sm:flex sm:flex-row gap-5 px-5 sm:px-16 py-5 h-[105vh]'>
+          <div className='sm:w-[70%] flex flex-col gap-5 w-full max-sm:hidden'>
+            <DetailsSection data={data} />
+            <ReviewSection />
+          </div>
+
+          <div className='max-sm:hidden w-[30%]'>
+            <BookingSection />
+          </div>
+
+          <div className='sm:w-[70%] flex flex-col gap-5 w-full sm:hidden'>
+            <DetailsSection data={data} />
+            <BookingSection />
+          </div>
+
+          <div className='sm:hidden'>
+            <ReviewSection />
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
--- components/utils/MultiSelectButtons.tsx
+++ components/utils/MultiSelectButtons.tsx
@@ -1,37 +1,45 @@
-import { Dispatch } from "react";
-import { Button } from "../ui/button";
-import { Clock2 } from "lucide-react";
+import { Dispatch } from 'react';
+import { Button } from '../ui/button';
+import { Clock2 } from 'lucide-react';
 
 interface MultiSelectButtonsProps {
-    options: { value: string; label: string, booked: boolean }[];
-    selected: string[];
-    onChange: Dispatch<React.SetStateAction<string[]>>;
+  options: { value: string; label: string; booked: boolean }[];
+  selected: string[];
+  onChange: Dispatch<React.SetStateAction<string[]>>;
 }
 
-export default function MultiSelectButtons({ options, selected = [], onChange }: MultiSelectButtonsProps) {
-    const toggleSelection = (value: string) => {
-        if (selected.includes(value)) {
-            onChange(selected.filter((item) => item !== value));
-        } else {
-            onChange([...selected, value]);
-        }
-    };
+export default function MultiSelectButtons({
+  options,
+  selected = [],
+  onChange,
+}: MultiSelectButtonsProps) {
+  const toggleSelection = (value: string) => {
+    if (selected.includes(value)) {
+      onChange(selected.filter((item) => item !== value));
+    } else {
+      onChange([...selected, value]);
+    }
+  };
 
-    return (
-        <>
-            {
-                options.map((option) => (
-                    <Button
-                        key={option.value}
-                        onClick={() => toggleSelection(option.value)}
-                        variant={(selected.includes(option.value) || option.booked) ? "primary" : "secondary"}
-                        className="mr-2 mb-2 text-xs w-40"
-                        disabled={option.booked}
-                    >
-                        <Clock2 height={15} /> {option.label}
-                    </Button>
-                ))
-            }
-        </>
-    );
-};
+  return (
+    <>
+      {options.map((option) => (
+        <Button
+          key={option.value}
+          onClick={() => toggleSelection(option.value)}
+          variant={
+            selected.includes(option.value) || option.booked
+              ? 'primary'
+              : 'secondary'
+          }
+          className='mr-2 mb-2 text-xs sm:w-40 '
+          disabled={option.booked}
+        >
+          <p className='flex'>
+            <Clock2 className='' height={15} /> {option.label}
+          </p>
+        </Button>
+      ))}
+    </>
+  );
+}
